eb09bc551f005ca3b1ea0ea4da626f0df2e82018
Namat <namatfvieceli@gmail.com>
2025-04-22 17:15:53 -0300

    Aula 7

---
+++ app/src/main/java/br/qi/aula06/MainActivity.kt
@@ -0,0 +1,180 @@
+package br.qi.aula06
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.qi.aula06.ui.theme.Aula06Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Aula06Theme {
+                //Aqui virá o código que criará o design no aparelho
+            }
+        }
+    }
+}
+
+@Composable //destinada a UI
+fun ProfileName(
+    @DrawableRes image : Int,
+    name : String,
+    time : String,
+    modifier: Modifier = Modifier //padrão para "editar"
+){
+    Row (
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = "Foto de perfil",
+            modifier = modifier
+                .size(40.dp)
+                .border(
+                    shape = CircleShape,
+                    width = 0.dp,
+                    color = Color.Transparent
+                )
+                .clip(CircleShape)
+        )
+
+        Spacer(
+            modifier = Modifier.padding(horizontal = 10.dp)
+        )
+
+        Column {
+            Text(
+                text = name,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = time,
+                color = Color.Gray
+            )
+
+        }
+    }
+}
+
+@Composable
+fun Post(
+    @DrawableRes image: Int,
+    modifier: Modifier = Modifier
+){
+    Box {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = "Imagem do post",
+            modifier = modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun PostIcons(
+    like : Boolean,
+    modifier: Modifier = Modifier
+){
+    Row {
+        IconButton(
+            onClick = { /*TODO*/ }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.FavoriteBorder,
+                contentDescription = "Ícone de curtida"
+            )
+        }
+
+        IconButton(
+            onClick = { /*TODO*/ }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Edit,
+                contentDescription = "Ícone de comentário"
+            )
+        }
+    }
+}
+
+@Composable
+fun PostText(
+    text : String,
+    modifier: Modifier = Modifier
+){
+    Box{
+        Text(
+            text = text,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+/////////////////////////////////////////////////////////////
+@Preview(showBackground = true)
+@Composable
+fun ProfileNamePreview() {
+    Aula06Theme {
+        ProfileName(
+            image = R.drawable.perry,
+            name = "Perry, o ornitorrinco",
+            time = "2 horas atrás"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PostPreview(){
+    Aula06Theme{
+        Post(image = R.drawable.perry)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PostIconsPreview(){
+    Aula06Theme {
+        PostIcons(like = true)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PostTextPreview(){
+    Aula06Theme {
+        PostText(text = "Segue um versinho maroto -> Atirei um limão na água, de pesado foi ao fundo, os peixinhos responderam: viva à Dom Pedro II! #poetadaqi #informatica")
+    }
+}
