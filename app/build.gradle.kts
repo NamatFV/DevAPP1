613da6d42689d3b7005b2f559a65e9f0f7ade908
Namat <namatfvieceli@gmail.com>
2025-05-06 17:02:22 -0300

    Aula 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "br.qi.aula06"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.qi.aula06"
-        minSdk = 28
-        targetSdk = 34
+        minSdk = 29
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/br/qi/aula06/LoginComposables.kt
@@ -0,0 +1,102 @@
+package br.qi.aula06
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material.icons.outlined.Done
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.qi.aula06.ui.theme.Aula06Theme
+
+@Composable
+fun LogoLogin(
+    @DrawableRes logo: Int,
+    modifier: Modifier = Modifier
+){
+    Image(
+        painter = painterResource(id = logo),
+        contentDescription = "Logotipo da tela de login",
+        modifier.height(150.dp)
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextField(
+    label: String,
+    isPassword: Boolean,
+    icon: ImageVector,
+    modifier: Modifier = Modifier
+){
+    var textValue by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+
+    var hidePass by remember {
+        mutableStateOf(true)
+    }
+
+    TextField(
+        value = textValue,
+        onValueChange = {textValue = it},
+        label = {
+            Text(text = label)
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = icon,
+                contentDescription = "Ícone da caixa de texto"
+            )
+        },
+        trailingIcon = {
+            if (isPassword){
+                IconButton(
+                    onClick = { hidePass = !hidePass }
+                ) {
+                    Icon(
+                        imageVector = if(hidePass) Icons.Outlined.Done else Icons.Outlined.Clear,
+                        contentDescription = "Ícone da caixa de texto"
+                    )
+                }
+            } else {
+                hidePass = false
+            }
+        },
+        visualTransformation = if(hidePass) PasswordVisualTransformation('?') else VisualTransformation.None
+    )//TextField
+
+}
+
+/////////////////////////////////////////////////////////////
+@Preview
+@Composable
+fun MyTextFieldPreview(){
+    Aula06Theme {
+        MyTextField(
+            label = "Senha",
+            isPassword = true,
+            icon = Icons.Outlined.Lock
+        )
+    }
+}
