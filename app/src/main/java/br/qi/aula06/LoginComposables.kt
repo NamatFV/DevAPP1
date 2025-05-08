a36ba515f4874e565c69eddb20b90eedaf7a8912
Namat <namatfvieceli@gmail.com>
2025-05-08 16:59:30 -0300

    Aula 12

--- app/src/main/java/br/qi/aula06/LoginComposables.kt
+++ app/src/main/java/br/qi/aula06/LoginComposables.kt
@@ -2,16 +2,24 @@ package br.qi.aula06
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Done
 import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
@@ -26,6 +35,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import br.qi.aula06.ui.theme.Aula06Theme
 
@@ -88,6 +98,66 @@ fun MyTextField(
 
 }
 
+@Composable
+fun MySwitch(
+    label: String,
+    isChecked: Boolean,
+    onChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+){
+    Row (
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Switch(
+            checked = isChecked,
+            onCheckedChange = onChange
+        )
+
+        Spacer(
+            modifier = modifier.width(6.dp)
+        )
+
+        Text(
+            text = label
+        )
+    }//Row
+}
+
+@Composable
+fun MySwitchState(
+    label: String,
+    modifier: Modifier = Modifier
+){
+    var checkChange by remember{
+        mutableStateOf(false)
+    }
+
+    MySwitch(
+        isChecked = checkChange,
+        onChange = {checkChange = !checkChange},
+        label = label
+    )
+}
+
+@Composable
+fun MyButton(
+    label: String,
+    action: () -> Unit = {},
+    size: Dp,
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier = modifier.width(size)
+    ){
+        ElevatedButton(
+            onClick = action,
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Text(text = label)
+        }
+    }
+}
+
 /////////////////////////////////////////////////////////////
 @Preview
 @Composable
@@ -98,5 +168,14 @@ fun MyTextFieldPreview(){
             isPassword = true,
             icon = Icons.Outlined.Lock
         )
+    }
+}
+
+@Preview
+@Composable
+fun MyComposablePreview(){
+    Column {
+        MySwitchState(label = "Texto qualquer")
+        MyButton(label = "Texto qualquer", size = 200.dp)
     }
 }
---
+++ app/src/main/java/br/qi/aula06/screens/Login.kt
@@ -0,0 +1,72 @@
+package br.qi.aula06.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.qi.aula06.LogoLogin
+import br.qi.aula06.MyButton
+import br.qi.aula06.MySwitchState
+import br.qi.aula06.MyTextField
+import br.qi.aula06.R
+import br.qi.aula06.ui.theme.Aula06Theme
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+
+    ){
+        LogoLogin(
+            logo = R.drawable.ic_launcher_foreground
+        )
+
+        MyTextField(
+            label = "Usuário",
+            isPassword = false,
+            icon = Icons.Outlined.AccountCircle
+        )
+
+        MyTextField(
+            label = "Senha",
+            isPassword = true,
+            icon = Icons.Outlined.Lock
+        )
+
+        MySwitchState(
+            label = "Mantenha-me conectado"
+        )
+
+        MyButton(
+            label = "Entrar",
+            size = 300.dp
+        )
+
+        MyButton(
+            label = "Registrar",
+            size = 300.dp
+        )
+    }//Column
+}
+
+/////////////////////////////////////////////////////////////
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 620
+)
+@Composable
+fun LoginScreenPreview(){
+    Aula06Theme {
+        LoginScreen()
+    }
+}
